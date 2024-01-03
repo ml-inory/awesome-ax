d9cbaa9b1878a4493ae4cdfbdaa5f312bbe153cc
祈Inory <584688538@qq.com>
2024-01-03 13:37:01 +0800

    add build_rtspserver.sh

---
+++ build_rtspserver.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+pwd=`pwd`
+thirdparty=${pwd}/thirdparty/RtspServer
+install_path=${pwd}/thirdparty-install/RtspServer
+
+if [[ ! -d ${thirdparty} ]]; then
+  echo "RtspServer not exist, please run git submodule update"
+  exit 1
+fi
+
+# RTSP
+cd ${thirdparty}
+mkdir RtspServer-build
+cd RtspServer-build
+cmake .. -DCMAKE_INSTALL_PREFIX=${install_path} -DCMAKE_TOOLCHAIN_FILE=${pwd}/toolchains/ax620e.cmake
+make -j8
+make install
+cd ${pwd}
